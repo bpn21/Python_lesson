b170b92c3c0c4f129400cae0bc7153db7b4226d2
bipin <bpngaire@gmail.com>
2021-02-10 11:30:11 +0545

    1st chapter of pyhton

---
+++ practice.py
@@ -0,0 +1,34 @@
+
+#We can use python in terminal . It is opens REPL - READ EVALUATE PRINT LOOP.
+#For multiple lines ko code ,We use IDE to write python code to make more convinent.
+
+
+# This is comment for Single lines .
+''' this is comment 
+for multiple 
+lines. '''
+
+#Tripple quote inside function >> we use for multiple lines
+print('''twinkie
+twinkle 
+little star''')
+
+
+
+#Using internal module
+import os  #Importing Os module
+print(os.listdir())
+
+#Using External Module
+'''For external module we use Python Pacakage Manager
+i.e Python Pacakage manager(pip)
+1st we install pacakage usng pacakage manager in terminal.
+EXAMPLE : 
+i.e pip install pacakage_name
+i.e pip install playsound'''
+
+from playsound import playsound 
+playsound('1.mp3')
+
+#Python worked as a music player . BINGO
+
