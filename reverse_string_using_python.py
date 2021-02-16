4901b3cd61fc84b1c7399dc8759704dcd5a54353
bipin <bpngaire@gmail.com>
2021-02-16 10:26:24 +0545

    reverse string

--- reverse_string_using_python.py
+++ reverse_string_using_python.py
@@ -1,14 +1,9 @@
 text = input("enter a text : ")
-print(type(text))
 length = len(text)
-print("length of text : ",length)
 count = 0
-reverse = ['','','','','']
+reverse = list()
 for i in range(length):
-    reverse[count] = text[length-count-1]
-    # array[count] = text[length-1]
+    reverse.append(text[length-count-1])
     count = count + 1
-    # length = length - 1
 
-print(type(reverse))
 print(reverse)
